d3b97aac4ed7bd8813374bb2ed3c65d92789503a
Miguelgargor <140601367+Miguelgargor@users.noreply.github.com>
2023-07-29 18:58:14 +0200

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -15,7 +15,7 @@ import mapclassify
 ##=========================================================================================================================================##
 
 ## TÍTULO de la PESTAÑA del NAVEGADOR y añadir ICONO: (TÍTULO, URL ICONO (Subido a GitHub))
-URL_ICONO= 'https://raw.githubusercontent.com/Miguelgargor/APP_CENTRALIDAD/main/Imagen1.png?token=GHSAT0AAAAAACFRVEI6SZGLQN42OSO72OEEZGE4AIQ'
+URL_ICONO= 'https://raw.githubusercontent.com/Miguelgargor/APP_CENTRALIDAD/main/Imagen1.png?token=GHSAT0AAAAAACFRVEI72HGG3M535XDOKMHIZGFIVJQ'
 st.set_page_config(page_title="ANÁLISIS CENTRALIDAD", page_icon=URL_ICONO, layout="centered", initial_sidebar_state="auto")
 ##===========================================================================================================================================##
 ##===========================================================================================================================================##
